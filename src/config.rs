d85379744d6774be924810328b5b4e28523532a2
fewensa <fewensa@protonmail.com>
2022-01-20 10:26:24 +0800

    Correct base path

--- src/config.rs
+++ src/config.rs
@@ -12,7 +12,12 @@ pub fn subquery_home() -> PathBuf {
     .map(|v| Path::new(&v).join(""))
     .ok()
     .or_else(dirs::home_dir)
-    .or_else(|| std::env::current_exe().ok())
+    .or_else(|| {
+      std::env::current_exe()
+        .map(|v| v.parent().map(|p| p.to_path_buf()))
+        .ok()
+        .unwrap_or_else(std::env::temp_dir)
+    })
     .unwrap_or_else(std::env::temp_dir);
   let mut base_path = basic_path;
   if !is_from_env {
